3492b52a95d5624e747bab700c19dec901b1e38e
Seth Goings <seth.goings@readytalk.com>
2014-12-17 14:06:36 -0700

    Update homesick configuration

--- roles/workstation.rb
+++ roles/workstation.rb
@@ -5,13 +5,15 @@ run_list(
   "role[base]",
   "recipe[chef-dk]",
   "recipe[vagrant]",
-  "recipe[sublime-text::default]",
-  "recipe[virtualbox]"
+  "recipe[sublime-text]",
+  "recipe[virtualbox]",
+  "recipe[homesick::data_bag]"
 )
 
 default_attributes(
   "vagrant" => {
     "url" => "https://dl.bintray.com/mitchellh/vagrant/vagrant_1.7.1_x86_64.deb",
     "checksum" => "6615b95fcd8044e2f5e1849ec1004df5e05e390812558ec2c4b3dcec541b92da"
-  }
+  },
+  "users" => ["sgoings"]
 )
